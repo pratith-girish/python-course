3f98e3c1b702310a81a431722ee2bbf943dc4454
pratith-girish <83457318+pratith-girish@users.noreply.github.com>
2021-05-02 08:09:56 -0700

    Create squarespiral6.py

---
+++ chapter2/squarespiral6.py
@@ -0,0 +1,12 @@
+# squarspiral6
+import turtle
+t = turtle.Pen()
+turtle.bgcolor("black")
+t.speed(0)
+sides = 3
+colors = ["red", "yellow", "blue", "orange", "green", "purple"]
+for x in range(360):
+    t.pencolor(colors[x%sides])
+    t.forward(x * 3 / sides + x)
+    t.left(360/sides + 1)
+    t.width(x*sides/100)
