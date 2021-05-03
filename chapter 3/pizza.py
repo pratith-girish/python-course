b492290becadf1e31b2493f5d6b0af48db1295b4
pratith-girish <83457318+pratith-girish@users.noreply.github.com>
2021-05-02 22:43:46 -0700

    Create pizza.py

---
+++ chapter 3/pizza.py
@@ -0,0 +1,15 @@
+# Atlantapizza.py
+
+number_of_pizza = eval(input("How many pizzas do you want?"))
+
+cost_per_pizza = eval(input("how much does each pizza cost"))
+
+Subtotal = number_of_pizzas * cost_per_pizza
+
+taxe_rate = 0.08
+
+sales_tax = subtotal + sales_tax
+
+print("the total cost is $", total)
+print("this includes $",subtotal,"for the pizza and")
+print("$", sales_tax,"in sales tax.")
