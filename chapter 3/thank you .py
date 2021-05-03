b81401be0135dbc580297f1ca1991cb0c4768a29
pratith-girish <83457318+pratith-girish@users.noreply.github.com>
2021-05-02 22:14:29 -0700

    Create thank you .py

---
+++ chapter 3/thank you .py
@@ -0,0 +1,8 @@
+# Thankyou.py
+my_name = "prathith"
+my_age = 12
+your_name = input("what is your name? ")
+your_age = input(" how old are you? ")
+print("My  name is",my_name, ",and i am",my_age,"years old.")
+print("your name is ", your_name, ",and you are",your_age,".")
+print("thank you for taking this course,",your_name,"!")
